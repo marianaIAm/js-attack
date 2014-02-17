9ad361a828a09da88119103fb657914d8c0e4a03
marianaIAm <mlenetis@gmail.com>
2014-02-16 22:54:47 -0800

    Add bad guys

--- app/js/main.js
+++ app/js/main.js
@@ -1,49 +1,83 @@
-var superMe;
+var superMe = new SuperMe();
+var canvasSuperMe = document.getElementById("super-me");
+var contextSuperMe = canvasSuperMe.getContext("2d");
+
 var canvasBackground = document.getElementById("canvas-background");
 var contextBackground = canvasBackground.getContext("2d");
 
-var canvasSuperMe = document.getElementById("canvas-super-me");
-var contextSuperMe = canvasSuperMe.getContext("2d");
+var canvasBadGuys = document.getElementById("bad-guys");
+var contextBadGuys = canvasBadGuys.getContext("2d");
+var badGuys = [];
+var spawnAmount = 5;
 
 var gameWidth = canvasBackground.width;
 var gameHeight = canvasBackground.height;
 
+var isPlaying = false;
 var fps = 10;
 var drawInterval;
+
+var requestAnimFrame = (function() {
+  return window.requestAnimationFrame ||
+         window.webkitRequestAnimationFrame ||
+         window.mozRequestAnimationFrame ||
+         window.msRequestAnimationFrame ||
+         window.oRequestAnimationFrame ||
+         function( callback ) {
+          window.setTimeout(callback, 1000 / 60);
+         };
+})();
 
 var imgSprite = new Image();
 imgSprite.src = "images/sprite.png";
 imgSprite.addEventListener("load", init, false);
 
 function init() {
+  spawnBadGuys(spawnAmount);
   drawBackground();
-  startDrawing();
-  superMe = new SuperMe();
+  startLoop();
   document.addEventListener("keydown", checkKeyDown, false);
   document.addEventListener("keyup", checkKeyUp, false);
 }
 
-function draw() {
-  superMe.draw();
+function loop() {
+  if (isPlaying) {
+    superMe.draw();
+    drawAllBadGuys();
+    requestAnimFrame(loop);
+  }
 }
 
-function startDrawing() {
-  stopDrawing();
-  drawInterval = setInterval(draw, fps);
+function startLoop() {
+  isPlaying = true;
+  loop();
 }
 
-function stopDrawing() {
-  clearInterval(drawInterval);
+function stopLoop() {
+  isPlaying = false;
+}
+
+function drawAllBadGuys() {
+  clearContextBadGuys();
+  for (var i = 0; i < badGuys.length; i++) {
+    badGuys[i].draw();
+  }
+}
+
+function spawnBadGuys(num) {
+  for (var i = 0; i < num; i++) {
+    badGuys[badGuys.length] = new BadGuy();
+  }
 }
 
 function SuperMe() {
   this.sourceX = 0;
   this.sourceY = 600;
-  this.drawX = 70;
-  this.drawY = 60;
   this.width = 146;
   this.height = 114;
   this.speed = 2;
+  this.drawX = 0;
+  this.drawY = 0;
   this.isUpKey = false;
   this.isRightKey = false;
   this.isDownKey = false;
@@ -71,6 +105,41 @@ SuperMe.prototype.checkKeys = function () {
   }
 }
 
+function clearContextSuperMe() {
+  contextSuperMe.clearRect(0, 0, gameWidth, gameHeight);
+}
+
+function BadGuy() {
+  this.sourceX = 0;
+  this.sourceY = 715;
+  this.width = 62;
+  this.height = 60;
+  this.speed = 2;
+  this.drawX = Math.floor(Math.random() * 800) + gameWidth;
+  this.drawY = Math.floor(Math.random() * (gameHeight - 150));
+}
+
+BadGuy.prototype.draw = function() {
+  this.drawX -= this.speed;
+  contextBadGuys.drawImage(imgSprite, this.sourceX, this.sourceY, this.width, this.height, this.drawX, this.drawY, this.width, this.height);
+  this.checkEscaped();
+};
+
+BadGuy.prototype.checkEscaped = function() {
+  if (this.drawX + this.width <= 0) {
+    this.reuseBadGuy();
+  }
+};
+
+BadGuy.prototype.reuseBadGuy = function() {
+  this.drawX = Math.floor(Math.random() * 800) + gameWidth;
+  this.drawY = Math.floor(Math.random() * (gameHeight - 150));
+};
+
+function clearContextBadGuys() {
+  contextBadGuys.clearRect(0, 0, gameWidth, gameHeight);
+}
+
 function drawBackground() {
   var sourceX = 0;
   var sourceY = 0;
@@ -81,10 +150,6 @@ function drawBackground() {
 
 function clearContextBackground() {
   contextBackground.clearRect(0, 0, gameWidth, gameHeight);
-}
-
-function clearContextSuperMe() {
-  contextSuperMe.clearRect(0, 0, gameWidth, gameHeight);
 }
 
 function checkKeyDown(e) {
